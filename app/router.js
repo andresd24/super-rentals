5d3d57802826a959d44628ff51e5567c3bb5799a
andresd24 <andresdev@gmail.com>
2017-05-12 04:40:51 -0500

    finish super rentals tutorial

--- app/router.js
+++ app/router.js
@@ -8,7 +8,9 @@ const Router = Ember.Router.extend({
 Router.map(function() {
   this.route('about');
   this.route('contact');
-  this.route('rentals');
+  this.route('rentals', function() {
+    this.route('show', { path: '/:rental_id' });
+  });
 });
 
 export default Router;
